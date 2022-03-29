4907f1238b5a7fb91cb236738e7d098e73e09c8a
Dmitriy <lozhkindm@yandex.ru>
2022-03-29 19:57:12 +0300

    response types

--- response-utils.go
+++ response-utils.go
@@ -2,7 +2,11 @@ package celeritas
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"fmt"
 	"net/http"
+	"path"
+	"path/filepath"
 )
 
 func (c *Celeritas) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
@@ -24,4 +28,52 @@ func (c *Celeritas) WriteJSON(w http.ResponseWriter, status int, data interface{
 		return err
 	}
 	return nil
+}
+
+func (c *Celeritas) WriteXML(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	res, err := xml.MarshalIndent(data, "", "   ")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.WriteHeader(status)
+
+	if _, err := w.Write(res); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Celeritas) DownloadFile(w http.ResponseWriter, r *http.Request, pathToFile, filename string) {
+	fullPath := path.Join(pathToFile, filename)
+	file := filepath.Clean(fullPath)
+	w.Header().Set("Content-Type", fmt.Sprintf("attachment; file=\"%s\"", filename))
+	http.ServeFile(w, r, file)
+}
+
+func (c *Celeritas) NotFound(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusNotFound)
+}
+
+func (c *Celeritas) InternalError(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusInternalServerError)
+}
+
+func (c *Celeritas) Unauthorized(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusUnauthorized)
+}
+
+func (c *Celeritas) Forbidden(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusForbidden)
+}
+
+func (c *Celeritas) ErrorStatus(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
 }
