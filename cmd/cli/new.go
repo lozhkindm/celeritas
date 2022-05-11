17bae2adcf07d33d771811fd8264b3e15280013d
Dmitriy <lozhkindm@yandex.ru>
2022-05-11 17:46:27 +0300

    new command app name sanitizing

--- cmd/cli/new.go
+++ cmd/cli/new.go
@@ -1,5 +1,13 @@
 package main
 
+import "strings"
+
 func doNew(appName string) error {
+	appName = strings.ToLower(appName)
+	if strings.Contains(appName, "/") {
+		parts := strings.Split(appName, "/")
+		appName = parts[len(parts)-1]
+	}
+
 	return nil
 }
