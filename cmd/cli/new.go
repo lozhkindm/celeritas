ae52840d327f31f79ea1c047968e5c94f6036db4
Dmitriy <lozhkindm@yandex.ru>
2022-05-11 18:35:36 +0300

    new command cloning repo

--- cmd/cli/new.go
+++ cmd/cli/new.go
@@ -1,6 +1,13 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/go-git/go-git/v5"
+)
 
 func doNew(appName string) error {
 	appName = strings.ToLower(appName)
@@ -9,5 +16,15 @@ func doNew(appName string) error {
 		appName = parts[len(parts)-1]
 	}
 
+	color.Green("\tCloning repository...")
+
+	_, err := git.PlainClone(fmt.Sprintf("./%s", appName), false, &git.CloneOptions{
+		URL:      "git@github.com/lozhkindm/celeritas-app",
+		Progress: os.Stdout,
+		Depth:    1,
+	})
+	if err != nil {
+		return err
+	}
 	return nil
 }
