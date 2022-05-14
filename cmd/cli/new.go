b9f8fa0e9a1413922fdc81191aab7daa00694b9c
Dmitriy <lozhkindm@yandex.ru>
2022-05-14 08:56:38 +0300

    new command creating .env

--- cmd/cli/new.go
+++ cmd/cli/new.go
@@ -26,5 +26,23 @@ func doNew(appName string) error {
 	if err != nil {
 		return err
 	}
+
+	if err := os.RemoveAll(fmt.Sprintf("./%s/.git", appName)); err != nil {
+		return err
+	}
+
+	color.Green("Creating .env file...")
+
+	contents, err := templateFS.ReadFile("templates/env.txt")
+	if err != nil {
+		return err
+	}
+	env := string(contents)
+	env = strings.ReplaceAll(env, "${APP_NAME}", appName)
+	env = strings.ReplaceAll(env, "${KEY}", cel.RandStr(32))
+	if err := copyDataToFile([]byte(env), fmt.Sprintf("./%s/.env", appName)); err != nil {
+		return err
+	}
+
 	return nil
 }
