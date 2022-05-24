9e33de3ce4695f50927bd4bbd97c150548d9690a
Dmitriy <lozhkindm@yandex.ru>
2022-05-24 19:09:33 +0300

    filesystem sftp put

--- filesystem/sftp/sftp.go
+++ filesystem/sftp/sftp.go
@@ -1,7 +1,14 @@
 package sftp
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"path"
+
 	"github.com/lozhkindm/celeritas/filesystem"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
 )
 
 type SFTP struct {
@@ -11,7 +18,54 @@ type SFTP struct {
 	Port     string
 }
 
+func (s *SFTP) getCredentials() (*sftp.Client, error) {
+	addr := fmt.Sprintf("%s:%s", s.Host, s.Port)
+	config := &ssh.ClientConfig{
+		User:            s.User,
+		Auth:            []ssh.AuthMethod{ssh.Password(s.Password)},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	conn, err := ssh.Dial("tcp", addr, config)
+	if err != nil {
+		return nil, err
+	}
+	client, err := sftp.NewClient(conn)
+	if err != nil {
+		return nil, err
+	}
+	wd, err := client.Getwd()
+	fmt.Println(wd)
+	return client, nil
+}
+
 func (s *SFTP) Put(filename, folder string) error {
+	client, err := s.getCredentials()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = client.Close()
+	}()
+
+	fileToUpload, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = fileToUpload.Close()
+	}()
+
+	fileSftp, err := client.Create(path.Base(filename))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = fileSftp.Close()
+	}()
+
+	if _, err := io.Copy(fileSftp, fileToUpload); err != nil {
+		return err
+	}
 	return nil
 }
 
