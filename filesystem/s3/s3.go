a599688f0087a96ebf58388aecd02fa11823b7c2
Dmitriy <lozhkindm@yandex.ru>
2022-05-29 11:28:46 +0300

    filesystem s3 list

--- filesystem/s3/s3.go
+++ filesystem/s3/s3.go
@@ -2,6 +2,11 @@ package s3
 
 import (
 	"github.com/lozhkindm/celeritas/filesystem"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 type S3 struct {
@@ -22,6 +27,29 @@ func (s *S3) Get(dst string, items ...string) error {
 
 func (s *S3) List(prefix string) ([]filesystem.ListEntry, error) {
 	var entries []filesystem.ListEntry
+	client := credentials.NewStaticCredentials(s.Key, s.Secret, "")
+	sess := session.Must(session.NewSession(&aws.Config{
+		Endpoint:    aws.String(s.Endpoint),
+		Region:      aws.String(s.Region),
+		Credentials: client,
+	}))
+	service := s3.New(sess)
+	input := &s3.ListObjectsInput{
+		Bucket: aws.String(s.Bucket),
+		Prefix: aws.String(prefix),
+	}
+	result, err := service.ListObjects(input)
+	if err != nil {
+		return nil, err
+	}
+	for _, content := range result.Contents {
+		entries = append(entries, filesystem.ListEntry{
+			Etag:         *content.ETag,
+			LastModified: *content.LastModified,
+			Key:          *content.Key,
+			Size:         float64(*content.Size) / 1024 / 1024,
+		})
+	}
 	return entries, nil
 }
 
